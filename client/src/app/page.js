e9bc5035ab2e167cfe97d201fd315b00c56e4dc2
avdhagam <davanisri@gmail.com>
2024-07-27 16:24:54 +0530

    added oAuth

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Room from "./pages/room";
 import UploadForm from "./components/uploadForm";
+import AuthPage from "./pages/auth";
 
 export default function Home() {
   return (
     <div>
-
-      <UploadForm />
-      <Room />
+      <AuthPage />
     </div>
 
 
